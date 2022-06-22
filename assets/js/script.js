5e58b370df2d1e72316911649cd36891fcdf65bb
MartinaP87 <bleach1987@hotmail.it>
2022-06-22 13:55:00 +0000

    Set up structure of the game functions

---
+++ assets/js/script.js
@@ -0,0 +1,67 @@
+
+  function pickObject() {
+    
+  }
+  /**
+   * Main function: it creates divs based on how many letters are in the word of the chosen object;
+   * each box has the respective letter in it.
+   */
+  function buildBoxes() {
+    
+  }
+  /**
+   * Function to check if the key pressed is included in the picked word; 
+   * if it is, it reveals it and trigger the victory function,  
+   * otherwise it starts to build the hangman;
+   */
+  function keyPressed(event) {
+    
+  } 
+  /**
+   * The function changes the image every time the answer is wrong;
+   * the last image ends the game, resulting in the user's failure.
+   */
+  function buildHangman() {
+    
+  }
+  
+  /**
+   *  Function that defines when the user wins 
+   */
+  function victory() {
+
+  }
+  /**
+   * Gets the current score of won games and increments it by 1
+   */
+  function incrementWon() {
+
+  }
+  /**
+  * Gets the current score of lost games and increments it by 1
+  */
+  function incrementLost() {
+    
+  }
+ /**
+  * Sets new styling properties when the game is over
+  */
+  function changeStyles() {
+    
+  }
+  /**
+   * Remove the chosen object from the wordsList array, 
+   * so it won't appear in the next game
+   */
+   function removeObject () {
+    
+  }
+  /**
+   * Restarts the game resetting the screen to the initial set up;
+   * sets new values to the global variables;
+   * calls the buildBoxes function (in which is called the pickWord function);
+   * and adds the event listener to the body.
+   */
+   function restart () {
+
+   }
